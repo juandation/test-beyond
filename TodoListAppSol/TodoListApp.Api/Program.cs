74c70660a8437069c37d566f6154fc3761f88e67
Juan David Ospina <juandation@gmail.com>
2025-04-14 08:47:56 +0200

    chore(api): config for api

--- TodoListAppSol/TodoListApp.Api/Program.cs
+++ TodoListAppSol/TodoListApp.Api/Program.cs
@@ -1,41 +1,32 @@
+using Carter;
+using TodoListApp.Core.Interfaces;
+using TodoListApp.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register Carter
+builder.Services.AddCarter();
+
+// Register Core Services (using Singleton for in-memory repository)
+builder.Services.AddSingleton<ITodoListRepository, InMemoryTodoListRepository>();
+builder.Services.AddScoped<ITodoList, TodoListService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Map Carter Endpoints
+app.MapCarter();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
